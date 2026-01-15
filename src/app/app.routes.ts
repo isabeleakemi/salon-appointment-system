8c9656d79aaca948f57fe13dedf3582700407818
isabeleakemi <isaahakemi@gmail.com>
2026-01-15 20:40:50 -0300

    Commit inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Agendamento } from './components/agendamento/agendamento';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: Agendamento
+    }
+];
